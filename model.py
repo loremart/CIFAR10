2492a5c659675702d0eaf6bbec532bc6e5d4ed4b
loremart <106522928+loremart@users.noreply.github.com>
2024-04-21 22:03:05 +0200

    1

--- model.py
+++ model.py
@@ -14,15 +14,16 @@ class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
 
 # Build the model
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
 model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10, activation='softmax'))
-
 # Compile the model
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
